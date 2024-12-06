b505204d3e3ada9c2aa55d3aa2e478c3263e4d0c
Matthieu Griffonnet <matthieu.griffonnet@etu.unice.fr>
2024-12-06 04:16:21 +0100

    Plastic invader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/js/Home';
+import Credits from './components/js/Credits';
 
 const App = () => {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+            <Route path="/credits" element={<Credits />} />
         </Routes>
       </Router>
   );
