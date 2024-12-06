7d5239d193b5a69a8e5e5255a4cb83058c2c483f
Rayan Outili <rayanout2005@gmail.com>
2024-12-06 02:04:03 +0100

    cookie cauchemar💀😱👻😈!!!!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/js/Home';
+import React, { useState } from 'react';
+import CookieButton from './components/CookieButton';
+import DarkSlider from './components/DarkSlider';
+import AnnoyingPopup from './components/AnnoyingPopup';
+import CookieConsent from './components/CookieConsent';
 
-const App = () => {
+function App() {
+  const [score, setScore] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const incrementScore = () => {
+    setScore((prevScore) => {
+      const newScore = prevScore + 1;
+
+      if (Math.random() > 0.8) {
+        setShowPopup(true);
+      }
+      return newScore;
+    });
+  };
+
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <div className="App" style={{ textAlign: 'center' }}>
+      <h1>Bienvenue au Cookie Cauchemar 🍪</h1>
+      <p>Score actuel : {score}</p>
+
+      {/* Bouton du cookie */}
+      <CookieButton onClick={incrementScore} />
+
+      <CookieConsent />
+
+      {/* Slider Chaotique */}
+      <DarkSlider />
+
+      {/* Popup en fonction de la probabilité */}
+      {showPopup && <AnnoyingPopup closePopup={() => setShowPopup(false)} />}
+    </div>
   );
-};
+}
 
 export default App;
